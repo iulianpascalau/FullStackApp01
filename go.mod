f607ce234bdfe82d5dc898e1050e2bb1a41c9868
Iulian Pascalau <iulian.pascalau@gmail.com>
2025-12-28 19:27:18 +0200

    - added backend interface as parameter in env file - integrated logger - refactored main

--- go.mod
+++ go.mod
@@ -7,14 +7,20 @@ toolchain go1.24.11
 require (
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/joho/godotenv v1.5.1
+	github.com/multiversx/mx-chain-logger-go v1.1.0
 	github.com/stretchr/testify v1.11.1
 	github.com/syndtr/goleveldb v1.0.0
+	github.com/urfave/cli v1.22.17
 	golang.org/x/crypto v0.46.0
 )
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.7 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
+	github.com/multiversx/mx-chain-core-go v1.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,35 +1,97 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
+	"runtime"
 
 	"FullStackApp01/api"
 	"FullStackApp01/storage"
 
 	"github.com/joho/godotenv"
+	"github.com/multiversx/mx-chain-logger-go"
+	"github.com/urfave/cli"
+)
+
+const helpTemplate = `NAME:
+   {{.Name}} - {{.Usage}}
+USAGE:
+   {{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}
+   {{if len .Authors}}
+AUTHOR:
+   {{range .Authors}}{{ . }}{{end}}
+   {{end}}{{if .Commands}}
+GLOBAL OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}
+VERSION:
+   {{.Version}}
+   {{end}}
+`
+
+var (
+	log    = logger.GetOrCreate("main")
+	cliApp *cli.App
 )
 
 func main() {
+	initCliFlags()
+
+	cliApp.Action = func(c *cli.Context) error {
+		return startApp(c)
+	}
+
+	err := cliApp.Run(os.Args)
+	if err != nil {
+		log.Error(err.Error())
+		os.Exit(1)
+	}
+}
+
+func initCliFlags() {
+	cliApp = cli.NewApp()
+	cli.AppHelpTemplate = helpTemplate
+	cliApp.Name = "App backend"
+	cliApp.Version = fmt.Sprintf("%s/%s/%s-%s", "1.0.1", runtime.Version(), runtime.GOOS, runtime.GOARCH)
+	cliApp.Usage = "App backend"
+	cliApp.Authors = []cli.Author{
+		{
+			Name:  "Iulian Pascalau",
+			Email: "iulian.pascalau@gmail.com",
+		},
+	}
+}
+
+func startApp(c *cli.Context) error {
+	log.Info("Starting app", "version", c.App.Version)
+
 	// Load .env file
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		return fmt.Errorf("error loading .env file: %w", err)
 	}
 
 	jwtKey := os.Getenv("JWT_KEY")
-	adminPassword := os.Getenv("ADMIN_PASSWORD")
+	if len(jwtKey) == 0 {
+		return errors.New("JWT_KEY is not set in the .env file")
+	}
 
-	if jwtKey == "" || adminPassword == "" {
-		log.Fatal("JWT_KEY and ADMIN_PASSWORD must be set in .env")
+	adminPassword := os.Getenv("ADMIN_PASSWORD")
+	if len(adminPassword) == 0 {
+		return errors.New("ADMIN_PASSWORD is not set in the .env file")
+	}
+
+	backendInterface := os.Getenv("BACKEND_INTERFACE")
+	if len(backendInterface) == 0 {
+		return errors.New("BACKEND_INTERFACE is not set in the .env file")
 	}
 
 	// Create or open a database in the "data" folder
 	store, err := storage.NewStore("data")
 	if err != nil {
-		log.Fatalf("Failed to open database: %v", err)
+		return fmt.Errorf("failed to open database: %w", err)
 	}
 	defer func() {
 		_ = store.Close()
@@ -45,10 +107,11 @@ func main() {
 	http.HandleFunc("/change-password", server.HandleChangePassword)
 	http.HandleFunc("/counter", server.HandleCounter)
 
-	port := ":8080"
-	fmt.Printf("Starting server on port %s\n", port)
-	err = http.ListenAndServe(port, nil)
+	log.Info("Starting server ", "interface", backendInterface)
+	err = http.ListenAndServe(backendInterface, nil)
 	if err != nil {
-		log.Fatalf("Could not start server: %s\n", err)
+		return fmt.Errorf("could not start server: %w", err)
 	}
+
+	return nil
 }
